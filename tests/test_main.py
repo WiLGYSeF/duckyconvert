88dd3f7758e7fa60bd3a250f70920a61259acad6
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-02-21 22:36:18 -0500

    move test files

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,4 +5,4 @@ import duckyconvert
 
 class MainTest(unittest.TestCase):
     def test_main(self):
-        duckyconvert.main(['-l', 'LED_BUILTIN', 'tests/convert_to_file/linux_test.txt', '-'])
+        duckyconvert.main(['-l', 'LED_BUILTIN', 'tests/convert_to_file/linux_test/linux_test.txt', '-'])
